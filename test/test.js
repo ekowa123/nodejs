d7846e16a7045a617f1bb7ee5b45b2175fa09f94
Ahmad Ardiansyah <ahmad.ardiansyah@carsworld.id>
2019-05-22 13:21:03 +0700

    add unit testing Node.js use mocha & chai

---
+++ test/test.js
@@ -0,0 +1,43 @@
+//During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
+
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../app.js');
+let should = chai.should();
+
+
+chai.use(chaiHttp);
+
+//Our parent block
+describe('Hello Node.js', () => {
+	
+	/*
+  * Test the /GET index
+  */
+  describe('/GET index', () => {
+      it('the title should be Ahmad Ardiansyah', (done) => {
+        chai.request(server)
+          .get('/').end((err, res) => {
+            res.should.have.status(200);
+          	done();
+          });
+      });
+  });
+
+  /*
+  * Test the /GET users
+  */
+  describe('/GET users', () => {
+      it('the response should be an JSON', (done) => {
+        chai.request(server)
+          .get('/users').end((err, res) => {
+            res.should.have.status(200);
+            res.should.be.json;
+            done();
+          });
+      });
+  });
+
+});
