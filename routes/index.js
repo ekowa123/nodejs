a8361a756262c3daccd14464ade9d8f7ab4a45e3
Ahmad Ardiansyah <ahmad.ardiansyah@carsworld.id>
2019-07-18 10:01:51 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Ahmad Ardiansyah' });
   res.json({status: 200, message: 'Welcome to Carsworld Digital Indonesia.'})
 });
 
 router.get('/ahmad', function(req, res, next) {
-  //res.render('index', { title: 'Ahmad Ardiansyah' });
   res.json({status: 200, message: 'Welcome Ahmad.'})
 });
 
 router.get('/ardi', function(req, res, next) {
-  //res.render('index', { title: 'Ahmad Ardiansyah' });
   res.json({status: 200, message: 'Welcome Ardi.'})
 });
 
 router.get('/ansyah', function(req, res, next) {
-  //res.render('index', { title: 'Ahmad Ardiansyah' });
   res.json({status: 200, message: 'Welcome Ansyah.'})
 });
 
 router.get('/bangke', function(req, res, next) {
-  //res.render('index', { title: 'Ahmad Ardiansyah' });
   res.json({status: 200, message: 'Welcome bangke.'})
 });
 
