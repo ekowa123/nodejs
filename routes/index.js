b8848b2a105fbb31275a6c7091adfec78b0c6902
Ahmad Ardiansyah <ahmad.ardiansyah@carsworld.id>
2019-09-19 12:50:21 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,8 @@ var client = new TelegramBotClient(TOKEN_BOT);
 
 var SECRET_KEY = '4hm4d_4rd14nsy4h_N0d3';
 
+tester = 'Hai';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { status: 200, message: 'Welcome to Carsworld Digital Indonesia.', title: 'Hello Node.js' });
