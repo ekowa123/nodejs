29f81b263fdb316e1260c7b4c8e73882189009ca
Ahmad Ardiansyah <ahmad.ardiansyah@carsworld.id>
2019-09-19 12:57:43 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,8 @@ var client = new TelegramBotClient(TOKEN_BOT);
 
 var SECRET_KEY = '4hm4d_4rd14nsy4h_N0d3';
 
-tester = 'Hai';
-tester2 = 'ahmad';
+var tester = 'Hai';
+var tester2 = 'ahmad';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
