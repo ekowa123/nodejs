801970fded8c811982be2452ee842296276e364c
Ahmad Ardiansyah <ahmad.ardiansyah@carsworld.id>
2019-09-19 13:06:53 +0800

    Update auth.js

--- configs/auth.js
+++ configs/auth.js
@@ -1,18 +1,15 @@
 // ini untuk jsonwebtokennya
 var jwt = require('jsonwebtoken');
-
 var SECRET_KEY = '4hm4d_4rd14nsy4h_N0d3';
-
 exports.authenticate = (req, res, next) => {
 	var token = req.headers['token'] || req.query.token;
 	if(!token) return res.json({ status: 500, message: 'No token provided'});
-
 	jwt.verify(token, SECRET_KEY, function(err, decoded) {
 		if(err) return res.json({ status: 500, message: 'Failed to authenticate token'});
 
 		req.app.token = decoded;
 		next();
+		return true;
 	});
-	
 	return true;
 }
