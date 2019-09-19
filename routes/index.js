49a75c1ee9c399f1892eb2949d5308683b6275f2
Ahmad Ardiansyah <ahmad.ardiansyah@carsworld.id>
2019-09-19 13:10:32 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -24,40 +24,36 @@ router.get('/auth/:user/:pass', function(req, res, next) {
 	if(!userParams) return res.json({ status: 500, message: 'Params user must be filled.'});
 	if(!passParams) return res.json({ status: 500, message: 'Params pass must be filled.'});
 
-	var token = jwt.sign(
-		{ user: userParams, pass: passParams }, 
-		SECRET_KEY, 
-		{ expiresIn: 120, algorithm: 'HS384' }
-	);
-
+	var token = jwt.sign({ user: userParams, pass: passParams }, SECRET_KEY, { expiresIn: 120, algorithm: 'HS384' });
 	res.json({ status: 200, message: 'Ok', data: token });
+	return true;
 });
 
 router.get('/me', auth.authenticate, function(req, res, next) {
 	console.log(req.app.token);
-	res.json({ status: 200, message: 'Ok, you are allowed'})
+	res.json({ status: 200, message: 'Ok, you are allowed'});
 });
 
 router.get('/api', function(req, res, next) {
 	if(req.query.stock){
 		client.sendMessage(324622115, 'Stok barang sekarang '+req.query.stock);
-  	res.json({status: 200, message: 'Stok barang sekarang '+req.query.stock})
+  	res.json({status: 200, message: 'Stok barang sekarang '+req.query.stock});
 	} else {
 		client.sendMessage(324622115, 'Stok barang habis');
-  	res.json({status: 200, message: 'Stok barang habis'})
+  	res.json({status: 200, message: 'Stok barang habis'});
 	}
 });
 
 router.get('/ahmad', function(req, res, next) {
-  res.json({status: 200, message: 'Welcome Ahmad.'})
+  res.json({status: 200, message: 'Welcome Ahmad.'});
 });
 
 router.get('/ardi', function(req, res, next) {
-  res.json({status: 200, message: 'Welcome Ardi.'})
+  res.json({status: 200, message: 'Welcome Ardi.'});
 });
 
 router.get('/ansyah', function(req, res, next) {
-  res.json({status: 200, message: 'Welcome Ansyah.'})
+  res.json({status: 200, message: 'Welcome Ansyah.'});
 });
 
 module.exports = router;
